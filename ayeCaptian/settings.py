78012441bdccdb930010da2a177174147b014f60
Pavan Sarla <sarlapavan2@gmail.com>
2026-02-19 19:34:43 +0530

    Db changed to Postgres

--- ayeCaptian/settings.py
+++ ayeCaptian/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,10 +81,11 @@ WSGI_APPLICATION = 'ayeCaptian.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+    'default': dj_database_url.config(
+        default='postgresql://clothing_ecommerce_user:N11k6MEOGHPkrJja3O9NbwVRpIAWldY5@dpg-d6bh6ufgi27c73daq90g-a.oregon-postgres.render.com/clothing_ecommerce',
+        conn_max_age=800,
+    )
 }
 
 
